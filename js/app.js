5c66daf89ae4ecf7de06f30722a3a4c81d02d920
Tomas Waples <tomas.waples@gmail.com>
2018-02-06 13:25:56 -0700

    Event Listners and Bootstrap work

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var listOfCards = [
-	'1 fa fa-diamond',
+let listOfCards = [
+	'1 fa-5x fa-diamond',
 	'2 fa fa-paper-plane-o',
 	'3 fa fa-anchor',
 	'4 fa fa-bolt',
@@ -17,12 +17,12 @@ var listOfCards = [
 	'16 fa fa-bomb',
 ];
 
-var i = '';
-var j = '';
-var k = '';
-var deckOfCards = document.getElementById('deck_of_cards');
-var listLength = listOfCards.length;
-var cardsHtml =  '';
+let i = '';
+let j = '';
+let k = '';
+let deckOfCards = document.getElementById('deck_of_cards');
+let listLength = listOfCards.length;
+let cardsHtml =  '<div class = "row">';
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -43,44 +43,135 @@ listOfCards=shuffle(listOfCards);
 
  for (i=0; i < listLength; i++){			
 	 
-	 cardsHtml += '<li class= "card" ><i id ="' + i +'" class = "card_down '+ listOfCards[i] + '"></i></li>';
+	 cardsHtml += '<div class= "card col-3"><div id ="' + i +'" class = "card_down '+ listOfCards[i] + '"></div></div>';
 
 	 deckOfCards.innerHTML = cardsHtml;
 		
 }
 
-var card_1 = document.getElementsByClassName('1');
-var card_9 = document.getElementsByClassName('9');
+const card_1 = document.getElementsByClassName('1');
+const card_2 = document.getElementsByClassName('2');
+const card_3 = document.getElementsByClassName('3');
+const card_4 = document.getElementsByClassName('4');
+const card_5 = document.getElementsByClassName('5');
+const card_6 = document.getElementsByClassName('6');
+const card_7 = document.getElementsByClassName('7');
+const card_8 = document.getElementsByClassName('8');
+const card_9 = document.getElementsByClassName('9');
+const card_10 = document.getElementsByClassName('10');
+const card_11 = document.getElementsByClassName('11');
+const card_12 = document.getElementsByClassName('12');
+const card_13 = document.getElementsByClassName('13');
+const card_14 = document.getElementsByClassName('14');
+const card_15 = document.getElementsByClassName('15');
+const card_16 = document.getElementsByClassName('16');
 
-var match_1 = 'closed';
-var match_9 = 'closed';
+let match_1 = 'closed';
+let match_2 = 'closed';
+let match_3 = 'closed';
+let match_4 = 'closed';
+let match_5 = 'closed';
+let match_6 = 'closed';
+let match_7 = 'closed';
+let match_8 = 'closed';
+let match_9 = 'closed';
+let match_10 = 'closed';
+let match_11 = 'closed';
+let match_12 = 'closed';
+let match_13 = 'closed';
+let match_14 = 'closed';
+let match_15 = 'closed';
+let match_16 = 'closed';
+
+let flip = function (x, y, z){
 
 
- function firstFlip(){
- 	var a = 'card_1';
- 	var b = '';
-
- 	a[0].classList.remove('card_down');
- 	b = 'open';
- }
-
-
- card_1[0].addEventListener('click', firstFlip (card_1, card_9));
-
-
+	x[0].classList.remove('card_down');
+	y = 'open'; 
+	setTimeout(function(){
+		if (z !== 'open'){
+			y = 'closed';}}, 5000);
+	setTimeout(function(){
+		if (z !== 'open'){
+			x[0].classList.add('card_down');}}, 5000);
+};
 
 card_1[0].addEventListener('click', function(){
-	card_1[0].classList.remove('card_down');
-	match_1 = 'open';});
+	
+	flip(card_1, match_1, match_9)
+});
+
+card_2[0].addEventListener('click', function(){
+	
+	flip(card_2, match_2, match_10)
+});
+
+card_3[0].addEventListener('click', function(){
+	
+	flip(card_3, match_3, match_11)
+});
+
+card_4[0].addEventListener('click', function(){
+	
+	flip(card_4, match_4, match_12)
+});
+
+card_5[0].addEventListener('click', function(){
+	
+	flip(card_5, match_5, match_13)
+});
+
+card_6[0].addEventListener('click', function(){
+	
+	flip(card_6, match_6, match_14)
+});
+
+card_7[0].addEventListener('click', function(){
+	
+	flip(card_7, match_7, match_15)
+});
+
+card_8[0].addEventListener('click', function(){
+	
+	flip(card_8, match_8, match_16)
+});
 
 card_9[0].addEventListener('click', function(){
-			card_9[0].classList.remove('card_down');
-			match_1 = 'open';});
-		
-		deckOfCards.addEventListener('click', function(){
-			if(match_1 === match_9 && match_9 === 'open'){
-				card_1[0].removeEventListner('click');}
-			else{
-				card_9[0].classList.add('card_down');
-				match_1 = 'closed';}
-			});
+	
+	flip(card_9, match_9, match_1)
+});
+
+card_10[0].addEventListener('click', function(){
+	
+	flip(card_10, match_10, match_2)
+});
+
+card_11[0].addEventListener('click', function(){
+	
+	flip(card_11, match_11, match_3)
+});
+
+card_12[0].addEventListener('click', function(){
+	
+	flip(card_12, match_12, match_4)
+});
+
+card_13[0].addEventListener('click', function(){
+	
+	flip(card_13, match_13, match_5)
+});
+
+card_14[0].addEventListener('click', function(){
+	
+	flip(card_14, match_14, match_6)
+});
+
+card_15[0].addEventListener('click', function(){
+	
+	flip(card_15, match_15, match_7)
+});
+
+card_16[0].addEventListener('click', function(){
+	
+	flip(card_16, match_16, match_8)
+});
